ed294ecefd75ad3b33867ed97bd1ac883a9e26d4
Jobin Kurian <jobin.kurian@netcorecloud.com>
2022-09-13 03:52:09 +0530

    v8.5.28

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SmartechNudgesSPM",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SmartechNudgesSPM",
+            targets: ["SmartechNudgesSPM", "SmartechNudges"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SmartechNudgesSPM", dependencies: []),
+        .binaryTarget(name: "SmartechNudges",
+                      url: "https://github.com/NetcoreSolutions/SmartechNudgesSPM/releases/download/8.5.28/SmartechNudges.xcframework.zip",
+                     checksum: "10f9847826eadb068efcffeff162c3578807c67524be1dfdc4fdbabb030bff03")
+    ]
+)
---
+++ README.md
@@ -0,0 +1,9 @@
+![Logo](https://netcorecloud.com/wp-content/themes/netcoretheme/assets/images/netcore-logo-main.svg)
+# SmartechNudges
+
+Smartech is a omni channel platform that delivers everything you need to drive mobile engagement and create valuable consumer relationships on mobile. The Smartech Nudges SDK enables your native iOS app to use Nudges along with other features of the Smartech SDK. This SDK works only in addition to Smartech-iOS-SDK. This guide contains all the information you need to integrate the SmartechNudges into your app.
+
+## Developer Docs
+To get started please refer to our developer docs to know how to make use of our SDK to trigger Nudges.
+
+[Developer Docs](https://docs.netcoresmartech.com/docs/ios-sdk-v3-nudges)
