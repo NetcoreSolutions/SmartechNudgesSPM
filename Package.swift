b998dc789814f5fdedaf7dc24db0bdadc474b040
Bhaskar Gangabathina <bhaskar.gangabattina@netcorecloud.com>
2026-07-03 14:38:57 +0530

    v9.0.35

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .target(name: "SmartechNudgesSPM", dependencies: []),
         .binaryTarget(name: "SmartechNudges",
-                      url: "https://github.com/NetcoreSolutions/SmartechNudgesSPM/releases/download/9.0.34/SmartechNudges.xcframework.zip",
-                     checksum: "d83e563815e9ddc6e6ee9910aa3619c1e2c64d4e53da9341664425238c5799eb")
+                      url: "https://github.com/NetcoreSolutions/SmartechNudgesSPM/releases/download/9.0.35/SmartechNudges.xcframework.zip",
+                     checksum: "f3df56c8023424a2dad01e49deeb86784ecd16d31b29da41c75749e1d4ad714c")
     ]
 )
